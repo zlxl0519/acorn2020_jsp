5ed4eb735b55c2ea43157801e0da5872fe23b3a0
seunghui <zlxl0519@naver.com>
2020-07-20 12:27:28 +0900

    Web07_Login 에 자료실 목록보기 페이징 처리중...

--- Web07_Login/src/test/file/dao/FileDao.java
+++ Web07_Login/src/test/file/dao/FileDao.java
@@ -20,6 +20,47 @@ public class FileDao {
 		}
 		return dao;
 	}
+	// 전체 row 의 갯수를 리턴해주는 메소드
+	public int getCount() {
+		//전체 row 의 갯수를 담을 지역 변수
+		int count=0;
+		//필요한 객체의 참조값을 담을 지역변수 만들기 
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			//Connection 객체의 참조값 얻어오기 
+			conn = new DbcpBean().getConn();
+			//rownum 중에서 가장 큰 숫자를 얻어오면 전체 row의 갯수가 된다.
+			//혹시 row 가 하나도 없으면 null 이 얻어와 지기때문에 null 인 경우 0 으로 
+			//바꿔 줘야 한다.
+			String sql = "select nvl(max(rownum), 0) as num"
+					+ "	from board_file";
+			pstmt = conn.prepareStatement(sql);
+			//sql 문에 ? 에 바인딩할 값이 있으면 바인딩하고 
+
+			//select 문 수행하고 결과 받아오기 
+			rs = pstmt.executeQuery();
+			//결과 값 추출하기 
+			if (rs.next()) {
+				count=rs.getInt("num");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return count;
+	}
+	
 	//인자로 전달되는 파일 번호에 해당하는 파일 정보를 삭제하는 메소드
 	public boolean delete(int num) {
 		Connection conn = null;
@@ -101,7 +142,7 @@ public class FileDao {
 	
 	
 	//파일 목록을 리턴해주는 메소드
-	public List<FileDto> getList(){
+	public List<FileDto> getList(FileDto dto){// dto 에 startRowNum, endRowNum 을 담아와서 쓴다.
 		//파일 목록을 담을 ArrayList 객체 생성
 		List<FileDto> list=new ArrayList<>();
 		//필요한 객체의 참조값을 담을 지역변수 만들기 
@@ -112,28 +153,31 @@ public class FileDao {
 			//Connection 객체의 참조값 얻어오기 
 			conn = new DbcpBean().getConn();
 			//실행할 sql 문 준비하기
-			String sql = "select num, writer, title, orgFileName,"
-					+ "saveFileName, fileSize, regdate"
-					+ "	from board_file"
-					+ "	order by num desc";
+			String sql = "select *"
+					+ "	from"
+					+ "		(select result1.*, rownum as rnum"
+					+ "		from (select num, writer, title, orgFileName, saveFileName,fileSize,regdate"
+					+ "		  		from board_file"
+					+ "		  		order by num desc) result1)"
+					+ "	where rnum between ? and ?";
 			pstmt = conn.prepareStatement(sql);
 			//sql 문에 ? 에 바인딩할 값이 있으면 바인딩하고 
-
+			pstmt.setInt(1, dto.getStartRowNum());
+			pstmt.setInt(2, dto.getEndRowNum());
 			//select 문 수행하고 결과 받아오기 
 			rs = pstmt.executeQuery();
 			//반복문 돌면서 결과 값 추출하기 
 			while (rs.next()) {
 				//select 된 파일의 정보를 FileDto 객체에 담고
-				FileDto dto=new FileDto();
-				dto.setNum(rs.getInt("num"));
-				dto.setWriter(rs.getString("writer"));
-				dto.setTitle(rs.getString("title"));
-				dto.setOrgFileName(rs.getString("orgFileName"));
-				dto.setSaveFileName(rs.getString("saveFileName"));
-				dto.setFileSize(rs.getLong("fileSize"));
-				dto.setRegdate(rs.getString("regdate"));
+				FileDto tmp=new FileDto();
+				tmp.setNum(rs.getInt("num"));
+				tmp.setWriter(rs.getString("writer"));
+				tmp.setTitle(rs.getString("title"));
+				tmp.setOrgFileName(rs.getString("orgFileName"));
+				tmp.setFileSize(rs.getLong("fileSize"));
+				tmp.setRegdate(rs.getString("regdate"));
 				//ArrayList 객체에 누적 시킨다.
-				list.add(dto);
+				list.add(tmp);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
