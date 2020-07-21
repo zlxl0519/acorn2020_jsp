313d9a8f3eef7c6cde124a253cc7d1f38cd203f0
seunghui <zlxl0519@naver.com>
2020-07-21 14:47:11 +0900

    Web07_Login 에 자료실 검색 기능 구현 완료

--- Web07_Login/WebContent/file/list.jsp
+++ Web07_Login/WebContent/file/list.jsp
@@ -1,3 +1,4 @@
+<%@page import="java.net.URLEncoder"%>
 <%@page import="test.file.dao.FileDao"%>
 <%@page import="java.util.List"%>
 <%@page import="test.file.dto.FileDto"%>
@@ -37,44 +38,81 @@
 	//로그인 된 아이디 읽어오기 (로그인을 하지 않으면 null 이다)
 	String id=(String)session.getAttribute("id");
 	
-		//한 페이지에 나타낼 row 의 갯수
-		final int PAGE_ROW_COUNT=5;
-		//하단 디스플레이 페이지 갯수
-		final int PAGE_DISPLAY_COUNT=5;
-		
-		//보여줄 페이지의 번호
-		int pageNum=1;
-		//보여줄 페이지의 번호가 파라미터로 전달되는지 읽어와 본다.	
-		String strPageNum=request.getParameter("pageNum");
-		if(strPageNum != null){//페이지 번호가 파라미터로 넘어온다면
-			//페이지 번호를 설정한다.
-			pageNum=Integer.parseInt(strPageNum);
+	//한 페이지에 나타낼 row 의 갯수
+	final int PAGE_ROW_COUNT=5;
+	//하단 디스플레이 페이지 갯수
+	final int PAGE_DISPLAY_COUNT=5;
+	
+	
+	
+	//보여줄 페이지의 번호
+	int pageNum=1;
+	//보여줄 페이지의 번호가 파라미터로 전달되는지 읽어와 본다.	
+	String strPageNum=request.getParameter("pageNum");
+	if(strPageNum != null){//페이지 번호가 파라미터로 넘어온다면
+		//페이지 번호를 설정한다.
+		pageNum=Integer.parseInt(strPageNum);
+	}
+	//보여줄 페이지 데이터의 시작 ResultSet row 번호
+	int startRowNum=1+(pageNum-1)*PAGE_ROW_COUNT; //공차 수열 an=a1+(n-1)d
+	//보여줄 페이지 데이터의 끝 ResultSet row 번호
+	int endRowNum=pageNum*PAGE_ROW_COUNT;
+	
+	/*
+	검색 키워드에 관련된 처리
+	*/
+	String keyword=request.getParameter("keyword");
+	if(keyword==null){//전달된 키워드가 없다면
+		keyword=""; //빈 문자열을 넣어준다.
+	}
+	//인코딩된 키워드를 미리 만들어 둔다.
+	String encodedK=URLEncoder.encode(keyword);
+	String condition=request.getParameter("condition");
+	//검색 키워드와 startRowNum, endRowNum 을 담을 FileDto 객체 생성
+	FileDto dto=new FileDto();
+	dto.setStartRowNum(startRowNum);
+	dto.setEndRowNum(endRowNum);
+	//select 된 결과를 담을 List
+	List<FileDto> list=null;
+	//전체 row 의 갯수를 담을 변수// 전체 row 의 갯수를 알아야지 페이지를 나눌수 있다.
+	int totalRow=0;
+	if(!keyword.equals("")){//만일 키워드가 넘어온다면
+		if(condition.equals("title_filename")){
+			//검색 키워드를 FileDto 객체의 필드에 담는다.
+			dto.setTitle(keyword);
+			dto.setOrgFileName(keyword);
+			//검색 키워드에 맞는 파일 목록중에서 pageNum 에 해당하는 목록 얻어오기
+			list=FileDao.getInstance().getListTF(dto);
+			//검색 키워드에 맞는 전체 글의 갯수를 얻어온다.
+			totalRow=FileDao.getInstance().getCountTF(dto);
+		}else if(condition.equals("title")){
+			dto.setTitle(keyword);
+			list=FileDao.getInstance().getListT(dto);
+			totalRow=FileDao.getInstance().getCountT(dto);
+		}else if(condition.equals("writer")){
+			dto.setWriter(keyword);
+			list=FileDao.getInstance().getListW(dto);
+			totalRow=FileDao.getInstance().getCountW(dto);
 		}
-		//보여줄 페이지 데이터의 시작 ResultSet row 번호
-		int startRowNum=1+(pageNum-1)*PAGE_ROW_COUNT; //공차 수열 an=a1+(n-1)d
-		//보여줄 페이지 데이터의 끝 ResultSet row 번호
-		int endRowNum=pageNum*PAGE_ROW_COUNT;
-		
-		//전체 row 의 갯수를 읽어온다.
-		int totalRow=FileDao.getInstance().getCount(); // 전체 row 의 갯수를 알아야지 페이지를 나눌수 있다.
-		//전체 페이지의 갯수 구하기
-		int totalPageCount=
-				(int)Math.ceil(totalRow/(double)PAGE_ROW_COUNT);
-		//시작 페이지 번호
-		int startPageNum=
-			1+((pageNum-1)/PAGE_DISPLAY_COUNT)*PAGE_DISPLAY_COUNT;
-		//끝 페이지 번호
-		int endPageNum=startPageNum+PAGE_DISPLAY_COUNT-1;
-		//끝 페이지 번호가 잘못된 값이라면 
-		if(totalPageCount < endPageNum){
-			endPageNum=totalPageCount; //보정해준다. 
-		}
-		//startRowNum 과 endRowNum을 FileDto 객체에 담고
-		FileDto dto=new FileDto();
-		dto.setStartRowNum(startRowNum);
-		dto.setEndRowNum(endRowNum);
-		//FileDto 객체를 인자로 전달해서 파일 목록을 얻어온다.
-		List<FileDto> list=FileDao.getInstance().getList(dto);
+	}else{//검색 키워드가 없으면 전체 목록을 얻어온다.
+		condition="";
+		keyword="";
+		list=FileDao.getInstance().getList(dto); // startRowNum, endRowNum 만 있는 list
+		totalRow=FileDao.getInstance().getCount();
+	}
+	
+	//전체 페이지의 갯수 구하기
+	int totalPageCount=
+			(int)Math.ceil(totalRow/(double)PAGE_ROW_COUNT);
+	//시작 페이지 번호
+	int startPageNum=
+		1+((pageNum-1)/PAGE_DISPLAY_COUNT)*PAGE_DISPLAY_COUNT;
+	//끝 페이지 번호
+	int endPageNum=startPageNum+PAGE_DISPLAY_COUNT-1;
+	//끝 페이지 번호가 잘못된 값이라면 
+	if(totalPageCount < endPageNum){
+		endPageNum=totalPageCount; //보정해준다. 
+	}
 %>
 <div class="container">
 	<a href="private/upload_form.jsp">파일 업로드</a>
@@ -112,21 +150,33 @@
 	</table>
 	<div class="page-display">
 		<ul>
-		<%if(startPageNum != 1) {%>
-			<li><a href="list.jsp?pageNum=<%=startPageNum-1 %>">Prev</a></li>
+		<%if(startPageNum != 1){ %>
+			<li><a href="list.jsp?pageNum=<%=startPageNum-1 %>&condition=<%=condition %>&keyword=<%=encodedK %>">Prev</a></li>
 		<%} %>
 		<%for(int i=startPageNum; i<=endPageNum; i++){ %>
 			<%if(i==pageNum){ %>
-				<li class="active"><a href="list.jsp?pageNum=<%=i %>"><%=i %></a></li>
-			<%}else{ %>
-				<li><a href="list.jsp?pageNum=<%=i %>"><%=i %></a></li>
+				<li class="active"><a href="list.jsp?pageNum=<%=i %>&condition=<%=condition %>&keyword=<%=encodedK %>"><%=i %></a></li>
+			<%}else{%>
+				<li><a href="list.jsp?pageNum=<%=i %>&condition=<%=condition %>&keyword=<%=encodedK %>"><%=i %></a></li>
 			<%} %>
-		<%} %>
-		<%if(endPageNum < totalPageCount) {%>
-			<li><a href="list.jsp?pageNum=<%=endPageNum+1 %>">Next</a></li>
 		<%} %>	
+		<%if(endPageNum < totalPageCount){ %>
+			<li><a href="list.jsp?pageNum=<%=endPageNum+1 %>&condition=<%=condition %>&keyword=<%=encodedK %>">Next</a></li>
+		<%} %>
 		</ul>
 	</div>
+	<hr style="clear:left; margin-top:10px"/>
+	<%-- 갖고 오는 목적이기 때문에 get 방식 , 제출하는 목적이면 post 방식 --%>
+	<form action="list.jsp" method="get">
+		<label for="condition">검색조건</label>
+		<select name="condition" id="condition">
+			<option value="title_filename" <%if(condition.equals("title_filename")){ %>selected<%} %>>제목+파일명</option>
+			<option value="title" <%if(condition.equals("title")){ %>selected<%} %>>제목</option>
+			<option value="writer" <%if(condition.equals("writer")){ %>selected<%} %>>작성자</option>
+		</select>
+		<input value="<%=keyword %>" type="text" name="keyword" placeholder="검색어..." />
+		<button type="submit">검색</button>
+	</form>
 </div>
 </body>
 </html>
--- Web07_Login/src/test/file/dao/FileDao.java
+++ Web07_Login/src/test/file/dao/FileDao.java
@@ -61,6 +61,133 @@ public class FileDao {
 		return count;
 	}
 	
+	//제목 파일명 검색결과 row 의 갯수를 리턴해주는 메소드
+	public int getCountTF(FileDto dto) {
+		//전체 row 의 갯수를 담을 지역 변수
+		int count=0;
+		//필요한 객체의 참조값을 담을 지역변수 만들기 
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			//Connection 객체의 참조값 얻어오기 
+			conn = new DbcpBean().getConn();
+			//rownum 중에서 가장 큰 숫자를 얻어오면 전체 row의 갯수가 된다.
+			//혹시 row 가 하나도 없으면 null 이 얻어와 지기때문에 null 인 경우 0 으로 
+			//바꿔 줘야 한다.
+			String sql = "select nvl(max(rownum), 0) as num"
+					+ "	from board_file"
+					+ "	where title like '%'||? ||'%' or orgFileName like '%'||?||'%'";
+			pstmt = conn.prepareStatement(sql);
+			//sql 문에 ? 에 바인딩할 값이 있으면 바인딩하고 
+			pstmt.setString(1, dto.getTitle());
+			pstmt.setString(2, dto.getOrgFileName());
+			//select 문 수행하고 결과 받아오기 
+			rs = pstmt.executeQuery();
+			//결과 값 추출하기 
+			if (rs.next()) {
+				count=rs.getInt("num");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return count;
+	}
+	
+	//제목 검색결과 row 의 갯수를 리턴해주는 메소드
+	public int getCountT(FileDto dto) {
+		//전체 row 의 갯수를 담을 지역 변수
+		int count=0;
+		//필요한 객체의 참조값을 담을 지역변수 만들기 
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			//Connection 객체의 참조값 얻어오기 
+			conn = new DbcpBean().getConn();
+			//rownum 중에서 가장 큰 숫자를 얻어오면 전체 row의 갯수가 된다.
+			//혹시 row 가 하나도 없으면 null 이 얻어와 지기때문에 null 인 경우 0 으로 
+			//바꿔 줘야 한다.
+			String sql = "select nvl(max(rownum), 0) as num"
+					+ "	from board_file"
+					+ "	where title like '%'||? ||'%'";
+			pstmt = conn.prepareStatement(sql);
+			//sql 문에 ? 에 바인딩할 값이 있으면 바인딩하고 
+			pstmt.setString(1, dto.getTitle());
+			//select 문 수행하고 결과 받아오기 
+			rs = pstmt.executeQuery();
+			//결과 값 추출하기 
+			if (rs.next()) {
+				count=rs.getInt("num");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return count;
+	}
+	
+	//작성자 검색결과 row 의 갯수를 리턴해주는 메소드
+	public int getCountW(FileDto dto) {
+		//전체 row 의 갯수를 담을 지역 변수
+		int count=0;
+		//필요한 객체의 참조값을 담을 지역변수 만들기 
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			//Connection 객체의 참조값 얻어오기 
+			conn = new DbcpBean().getConn();
+			//rownum 중에서 가장 큰 숫자를 얻어오면 전체 row의 갯수가 된다.
+			//혹시 row 가 하나도 없으면 null 이 얻어와 지기때문에 null 인 경우 0 으로 
+			//바꿔 줘야 한다.
+			String sql = "select nvl(max(rownum), 0) as num"
+					+ "	from board_file"
+					+ "	where writer like '%'|| ? ||'%'";
+			pstmt = conn.prepareStatement(sql);
+			//sql 문에 ? 에 바인딩할 값이 있으면 바인딩하고 
+			pstmt.setString(1, dto.getWriter());
+			//select 문 수행하고 결과 받아오기 
+			rs = pstmt.executeQuery();
+			//결과 값 추출하기 
+			if (rs.next()) {
+				count=rs.getInt("num");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return count;
+	}
+		
 	//인자로 전달되는 파일 번호에 해당하는 파일 정보를 삭제하는 메소드
 	public boolean delete(int num) {
 		Connection conn = null;
@@ -156,7 +283,7 @@ public class FileDao {
 			String sql = "select *"
 					+ "	from"
 					+ "		(select result1.*, rownum as rnum"
-					+ "		from (select num, writer, title, orgFileName, saveFileName,fileSize,regdate"
+					+ "		from (select num, writer, title, orgFileName, fileSize,regdate"
 					+ "		  		from board_file"
 					+ "		  		order by num desc) result1)"
 					+ "	where rnum between ? and ?";
@@ -194,7 +321,174 @@ public class FileDao {
 		}
 		return list;
 	}
+		//파일 목록을 리턴해주는 메소드 제목과 파일명 검색
+		public List<FileDto> getListTF(FileDto dto){
+			//파일 목록을 담을 ArrayList 객체 생성
+			List<FileDto> list=new ArrayList<>();
+			//필요한 객체의 참조값을 담을 지역변수 만들기 
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			try {
+				//Connection 객체의 참조값 얻어오기 
+				conn = new DbcpBean().getConn();
+				//실행할 sql 문 준비하기
+				String sql = "select *"
+						+ "	from"
+						+ "		(select result1.*, rownum as rnum"
+						+ "		from (select num, writer, title, orgFileName,fileSize,regdate"
+						+ "		  		from board_file"
+						+ "				where title like '%'||?||'%' or orgFileName like '%'||?||'%'"
+						+ "		  		order by num desc) result1)"
+						+ "	where rnum between ? and ?";
+				pstmt = conn.prepareStatement(sql);
+				//sql 문에 ? 에 바인딩할 값이 있으면 바인딩하고 
+				pstmt.setString(1, dto.getTitle());
+				pstmt.setString(2, dto.getOrgFileName());
+				pstmt.setInt(3, dto.getStartRowNum());
+				pstmt.setInt(4, dto.getEndRowNum());
+				//select 문 수행하고 결과 받아오기 
+				rs = pstmt.executeQuery();
+				//반복문 돌면서 결과 값 추출하기 
+				while (rs.next()) {
+					//select 된 파일의 정보를 FileDto 객체에 담고
+					FileDto tmp=new FileDto();
+					tmp.setNum(rs.getInt("num"));
+					tmp.setWriter(rs.getString("writer"));
+					tmp.setTitle(rs.getString("title"));
+					tmp.setOrgFileName(rs.getString("orgFileName"));
+					tmp.setFileSize(rs.getLong("fileSize"));
+					tmp.setRegdate(rs.getString("regdate"));
+					//ArrayList 객체에 누적 시킨다.
+					list.add(tmp);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (rs != null)
+						rs.close();
+					if (pstmt != null)
+						pstmt.close();
+					if (conn != null)
+						conn.close();
+				} catch (Exception e) {
+				}
+			}
+			return list;
+		}
 	
+		//파일 목록을 리턴해주는 메소드 제목으로 검색
+		public List<FileDto> getListT(FileDto dto){
+			//파일 목록을 담을 ArrayList 객체 생성
+			List<FileDto> list=new ArrayList<>();
+			//필요한 객체의 참조값을 담을 지역변수 만들기 
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			try {
+				//Connection 객체의 참조값 얻어오기 
+				conn = new DbcpBean().getConn();
+				//실행할 sql 문 준비하기
+				String sql = "select *"
+						+ "	from"
+						+ "		(select result1.*, rownum as rnum"
+						+ "		from (select num, writer, title, orgFileName, fileSize,regdate"
+						+ "		  		from board_file"
+						+ "				where title like '%'||?||'%'"
+						+ "		  		order by num desc) result1)"
+						+ "	where rnum between ? and ?";
+				pstmt = conn.prepareStatement(sql);
+				//sql 문에 ? 에 바인딩할 값이 있으면 바인딩하고 
+				pstmt.setString(1, dto.getTitle());
+				pstmt.setInt(2, dto.getStartRowNum());
+				pstmt.setInt(3, dto.getEndRowNum());
+				//select 문 수행하고 결과 받아오기 
+				rs = pstmt.executeQuery();
+				//반복문 돌면서 결과 값 추출하기 
+				while (rs.next()) {
+					//select 된 파일의 정보를 FileDto 객체에 담고
+					FileDto tmp=new FileDto();
+					tmp.setNum(rs.getInt("num"));
+					tmp.setWriter(rs.getString("writer"));
+					tmp.setTitle(rs.getString("title"));
+					tmp.setOrgFileName(rs.getString("orgFileName"));
+					tmp.setFileSize(rs.getLong("fileSize"));
+					tmp.setRegdate(rs.getString("regdate"));
+					//ArrayList 객체에 누적 시킨다.
+					list.add(tmp);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (rs != null)
+						rs.close();
+					if (pstmt != null)
+						pstmt.close();
+					if (conn != null)
+						conn.close();
+				} catch (Exception e) {
+				}
+			}
+			return list;
+		}
+	
+		//파일 목록을 리턴해주는 메소드 //작성자 검색
+		public List<FileDto> getListW(FileDto dto){
+			//파일 목록을 담을 ArrayList 객체 생성
+			List<FileDto> list=new ArrayList<>();
+			//필요한 객체의 참조값을 담을 지역변수 만들기 
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			try {
+				//Connection 객체의 참조값 얻어오기 
+				conn = new DbcpBean().getConn();
+				//실행할 sql 문 준비하기
+				String sql = "select *"
+						+ "	from"
+						+ "		(select result1.*, rownum as rnum"
+						+ "		from (select num, writer, title, orgFileName, fileSize,regdate"
+						+ "		  		from board_file"
+						+ "				where writer like '%'||?||'%'"
+						+ "		  		order by num desc) result1)"
+						+ "	where rnum between ? and ?";
+				pstmt = conn.prepareStatement(sql);
+				//sql 문에 ? 에 바인딩할 값이 있으면 바인딩하고
+				pstmt.setString(1, dto.getWriter());
+				pstmt.setInt(2, dto.getStartRowNum());
+				pstmt.setInt(3, dto.getEndRowNum());
+				//select 문 수행하고 결과 받아오기 
+				rs = pstmt.executeQuery();
+				//반복문 돌면서 결과 값 추출하기 
+				while (rs.next()) {
+					//select 된 파일의 정보를 FileDto 객체에 담고
+					FileDto tmp=new FileDto();
+					tmp.setNum(rs.getInt("num"));
+					tmp.setWriter(rs.getString("writer"));
+					tmp.setTitle(rs.getString("title"));
+					tmp.setOrgFileName(rs.getString("orgFileName"));
+					tmp.setFileSize(rs.getLong("fileSize"));
+					tmp.setRegdate(rs.getString("regdate"));
+					//ArrayList 객체에 누적 시킨다.
+					list.add(tmp);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (rs != null)
+						rs.close();
+					if (pstmt != null)
+						pstmt.close();
+					if (conn != null)
+						conn.close();
+				} catch (Exception e) {
+				}
+			}
+			return list;
+		}
 	// 파일 정보를 DB에 저장하는 메소드
 	public boolean insert(FileDto dto) {
 		Connection conn = null;
